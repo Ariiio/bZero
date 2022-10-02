566435ae8adda1354df5b91524b9c0c60716b7ed
Ariiio <77200189+Ariiio@users.noreply.github.com>
2022-10-02 21:18:55 +0200

    lets go

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <time.h>
+
+void delay_us(unsigned int micro);
 
 int main()
 {
     printf("Test");
 
     return 0;
+}
+
+void delay_us(unsigned int micro)
+{
+    clock_t goal = micro / 1000 + clock();
+    while (goal > clock());
 }
